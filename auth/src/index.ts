9ab714743040655d1fe3838c9a5a7669030092b5
Arunkumar Narayanaswamy <arunkmr1983@gmail.com>
2022-05-17 13:31:27 +0100

    implemented error handling for auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,13 @@
 const express = require('express')
+import 'express-async-errors'
 import { json } from 'body-parser'
+import { NotFoundError } from './errors/not-found-error'
+import { errorHandler } from './middlewares/error-handler'
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
 import { signupRouter } from './routes/signup'
+
 const app = express()
 app.use(json())
 const port = 3000
@@ -12,6 +16,11 @@ app.use(currentUserRouter)
 app.use(signinRouter)
 app.use(signoutRouter)
 app.use(signupRouter)
+
+app.all('*', async () => {
+  throw new NotFoundError()
+})
+app.use(errorHandler)
 
 app.listen(port, () =>
   console.log(`Example app listening on port ${port}!!!!!`)
