b9e7633462c407f7d37b64cdd8d2a8164ff24374
Arunkumar Narayanaswamy <arunkmr1983@gmail.com>
2022-05-18 15:30:58 +0100

    implemented user signup api

--- auth/src/models/users.ts
+++ auth/src/models/users.ts
@@ -1,4 +1,19 @@
 import mongoose from 'mongoose'
+import { Password } from '../services/password'
+
+interface UserAttr {
+  email: string
+  password: string
+}
+
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttr): UserDoc
+}
+
+interface UserDoc extends mongoose.Document {
+  email: string
+  password: string
+}
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -11,6 +26,18 @@ const userSchema = new mongoose.Schema({
   },
 })
 
-const User = mongoose.model('User', userSchema)
+//quick reminder this function keyword helps to use this keyword inside the method. if you use arrow function, then this keyword is not related
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashedPassword = await Password.toHash(this.get('password'))
+    this.set('password', hashedPassword)
+  }
+  done()
+})
+
+userSchema.statics.build = (attrs: UserAttr) => {
+  return new User(attrs)
+}
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema)
 
 export { User }
