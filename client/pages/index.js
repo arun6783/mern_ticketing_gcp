9bc5d29f1d31f4192f8f86207097f769ac573c36
Arunkumar Narayanaswamy <arunkmr1983@gmail.com>
2022-05-20 20:16:03 +0100

    implemented redirect from signup to landing with cookies set

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import buildClient from '../api/build-client'
 
-const LandingPage = () => {
-  return <div>Landing</div>
+const LandingPage = ({ currentUser }) => {
+  // console.log(currentUser);
+  // axios.get('/api/users/currentuser');
+  console.log(currentUser)
+
+  return <h1>Landing Page</h1>
 }
 
-LandingPage.getInitialProps = () => {}
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context)
+  const { data } = await client.get('/api/users/currentuser')
+
+  return data
+}
 
 export default LandingPage
