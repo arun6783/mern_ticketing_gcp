06507d26f039346bf9f515150d574b11fc958468
Arunkumar Narayanaswamy <arunkmr1983@gmail.com>
2022-05-20 11:25:39 +0100

    refactored test

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,35 +2,20 @@ import request from 'supertest'
 import { app } from '../../app'
 
 it('responds with details about the current user', async () => {
-  const signupResponse = await request(app)
-    .post('/api/users/signup')
-    .send({
-      email: 'test@test.com',
-      password: 'password',
-    })
-    .expect(201)
-  const signupCookie = signupResponse.get('Set-Cookie')
+  const cookie = await global.signin()
 
   const response = await request(app)
-    .post('/api/users/currentuser')
-    .set('Cookie', signupCookie)
+    .get('/api/users/currentuser')
+    .set('Cookie', cookie)
     .send()
     .expect(200)
 
   expect(response.body.currentUser.email).toEqual('test@test.com')
 })
 
-it('responds with null if not authenticated', async () => {
-  const signupResponse = await request(app)
-    .post('/api/users/signup')
-    .send({
-      email: 'test@test.com',
-      password: 'password',
-    })
-    .expect(201)
+it('responds with 401 if not authenticated', async () => {
   const response = await request(app)
-    .post('/api/users/currentuser')
+    .get('/api/users/currentuser')
     .send()
-    .expect(200)
-  expect(response.body.currentUser.email).toEqual('test@test.com')
+    .expect(401)
 })
