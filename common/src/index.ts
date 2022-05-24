c24d024c9849c20a6adb78700213c4f03e3010c1
Arunkumar Narayanaswamy <arunkmr1983@gmail.com>
2022-05-24 09:47:36 +0100

    Updates

---
+++ common/src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+  red: number
+  blue: number
+  green: number
+}
+
+const color: Color = {
+  red: 22,
+  blue: 1220,
+  green: 20,
+}
+
+console.log(color)
+
+export default color
