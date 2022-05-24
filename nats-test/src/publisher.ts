5750f7de328cc5d5ce26ebacb529c495d1dd39b6
Arunkumar Narayanaswamy <arunkmr1983@gmail.com>
2022-05-24 09:22:42 +0100

    created simple nats pub and listener

---
+++ nats-test/src/publisher.ts
@@ -0,0 +1,19 @@
+import { connect, Message } from 'node-nats-streaming'
+console.clear()
+const stan = connect('ticketing', 'abc', {
+  url: 'http://localhost:4222',
+})
+
+stan.on('connect', () => {
+  console.log('publisher connected to nats')
+
+  const data = JSON.stringify({
+    id: '123',
+    title: 'concert',
+    price: 20,
+  })
+
+  stan.publish('ticket:created', data, () => {
+    console.log('event published')
+  })
+})
