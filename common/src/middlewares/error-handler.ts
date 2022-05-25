723e46575a041bfd2b62e1fac80a1fd2e5dc8578
Arunkumar Narayanaswamy <arunkmr1983@gmail.com>
2022-05-25 17:31:52 +0100

    fixing error handler

--- common/src/middlewares/error-handler.ts
+++ common/src/middlewares/error-handler.ts
@@ -12,6 +12,7 @@ export const errorHandler = (
   if (err instanceof CustomError) {
     return res.status(err.statusCode).send(err.serializeErrors())
   }
+  console.error(err)
   res.status(500).send({
     errors: [{ message: 'Something went wrong!!!' }],
   })
