8a7fbab1a06cfa06ccb3e4d74fe9c0f408a153d0
Arunkumar Narayanaswamy <arunkmr1983@gmail.com>
2022-06-07 17:00:05 +0100

    added new test for payments route

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -36,4 +36,24 @@ it('returns a 401 when purchasing an ordder that doest belong to the user', asyn
     .expect(401)
 })
 
-it('returns a 400 when purchasing a cancelled order', async () => {})
+it('returns a 400 when purchasing a cancelled order', async () => {
+  const userId = new mongoose.Types.ObjectId().toHexString()
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    userId: userId,
+    version: 0,
+    price: 20,
+    status: OrderStatus.Cancelled,
+  })
+
+  await order.save()
+
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signin(userId))
+    .send({
+      token: 'asddad',
+      orderId: order.id,
+    })
+    .expect(400)
+})
--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -5,7 +5,7 @@ import { app } from '../app'
 import jwt from 'jsonwebtoken'
 
 declare global {
-  var signin: () => string[]
+  var signin: (id?: string) => string[]
 }
 jest.mock('../nats-wrapper')
 
@@ -37,10 +37,11 @@ afterAll(async () => {
   mongoose.connection.close()
 })
 
-global.signin = () => {
+global.signin = (id = new mongoose.Types.ObjectId().toHexString()) => {
+  console.log('usersign id -', id)
   // Build a JWT payload.  { id, email }
   const payload = {
-    id: new mongoose.Types.ObjectId().toHexString(),
+    id: id,
     email: 'test@test.com',
   }
 
